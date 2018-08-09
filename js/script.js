6a9eb1b49ee43562f311eb8bad7a40b8068e5334
ctjsctjs <ctjsctjs@gmail.com>
2018-08-09 12:27:10 +0800

    Added filter and hover functions

--- js/script.js
+++ js/script.js
@@ -1,65 +1,154 @@
 //Global var
 var adapterCount=0;
+var hiddenCount=0;
+var json;
 
 //On Init
 $( document ).ready(function() {
+  loadJSON();
   generateAdapters();
+  filterHandler();
 });
+
+//Handle filter navigation clicks
+function filterHandler(){
+  $("body").on("click", ".filter-section ul li", function(){
+
+    //Clear active navigation
+    $("#container-filter").find("li").removeClass("filter-active");
+
+    //Set active navigation
+    $(this).addClass("filter-active");
+
+    //Get filter and value selected
+    var filter = ($(this).parent().siblings("h5").html());
+    var val = $(this).html();
+
+    //Toggle hidden according to filter match
+    $('.app').each(function() {
+      var name = $(this).find(".item-name").html();
+      var properties = json[name].properties;
+
+      if (properties[filter]!=val){
+        $(this).addClass("filter-hidden");
+      } else {
+        $(this).removeClass("filter-hidden");
+      }
+    })
+    checkAllHidden();
+  });
+}
+
+//Load JSON
+function loadJSON(){
+  $.ajax({
+      'async': false,
+      'global': false,
+      'url': 'json/adapters.json',
+      'dataType': "json",
+      'success': function (data) {
+          json = data;
+      }
+  });
+}
 
 //Search Function
 function filterFunc(event) {
-
-  var tempCount=0;
   var userInput = event.value.toUpperCase();
-
   $('.app').each(function() {
     if (this.textContent.toUpperCase().indexOf(userInput) > -1) {
-      $(this).show();
+      $(this).removeClass("search-hidden");
     } else {
-      $(this).hide();
-
-      tempCount += 1;
-    }
-    //Show no result message if all adapters are hidden
-    if (tempCount == adapterCount){
-      $('#no-result').show();
-    } else {
-      $('#no-result').hide();
+      $(this).addClass("search-hidden");
     }
   });
+  checkAllHidden();
+}
+
+//Function to check if all apps are hidden, show no results message
+function checkAllHidden(){
+  //Show no result message if all adapters are hidden
+  var valid = true;
+  $('.app').each(function() {
+    if ($(this).attr("class")==="app"){
+      valid = false;
+    }
+
+    if (valid==false){
+      $('#no-result').hide();
+      $(".splitter").show();
+
+    } else {
+      $('#no-result').show();
+      $(".splitter").hide();
+    }
+  })
 }
 
 //Load JSON file to generate app listing
 function generateAdapters(){
 
-  //Load JSON file
-  $.getJSON('adapters/adapters.json', function (json) {
-    var listCol = 5;
-    var container = $('#container-apps');
+  generateCount = 0;
+  var filters={};
+  var listCol = 5;
+  var ul = $('#container-apps');
 
-    //Generate app list from adapters.JSON
-    json.adapters.forEach(function (adapter) {
+  //Generate app list from adapters.JSON
+  Object.keys(json).forEach(function (key) {
+    //Count adapter for no result function
+    adapterCount += 1;
 
-      //Count adapter for no result function
-      adapterCount += 1;
+    var link = json[key].link;
+    var git = json[key].git;
+    var img = json[key].image;
+    var name = json[key].name;
+    var properties = json[key].properties;
 
-      var link = adapter.link;
-      var img = adapter.image;
-      var name = adapter.name;
+    //Load filter options
+    $.each(properties, function(i, item) {
+      if (!(i in filters)){
+        filters[i]=[];
+      }
+      if (!(filters[i].includes(item))){
+        filters[i].push(item);
+      }
+    })
 
-      var content = '<li class="app">' +
-      '<a target="_blank" href="' + link + '">' +
-      '<img src="' + img + '">' +
-      '<div class="item-name black-font">' + name + '</div>';
+    var li = $("<li>").attr({"class": "app"}).appendTo(ul);
+    var a = $("<a>").attr({
+      "href": "javascript:void(0);",
+      "target":"_blank",
+      "class":"app-link",
+    }).appendTo(li);
 
-      $(content).appendTo(container);
-    });
-
-    //Generate dummy box for responsive
-    for (var i = 0; i < (listCol); i++) {
-      $('<li class="item flex-dummy"></li>').appendTo(container);
-    }
+    var image = $("<img>").attr("src", img).appendTo(a);
+    var label = $("<div>").attr("class", "item-name black-font").html(name).appendTo(a);
+    var hoverContainer = $("<div>").attr("class", "hoverContainer").appendTo(li);
+    var hoverUL = $("<ul>").appendTo(hoverContainer);
+    var hoverPageLi = $("<li>").attr("class", name).appendTo(hoverUL);
+    var hoverPageA = $("<a>").attr({"href": link, "target":"_blank", "class":"app-link"}).html("Install Connector").appendTo(hoverPageLi);
+    var hoverGitLi = $("<li>").appendTo(hoverUL);
+    var hoverGitA = $("<a>").attr({"href": git, "target":"_blank", "class":"app-link"}).html("View Source Code").appendTo(hoverGitLi);
   });
+
+  //Generate dummy box for responsive
+  for (var i = 0; i < (listCol); i++) {
+    $('<li class="item flex-dummy"></li>').appendTo(ul);
+  }
+  generateFilters(filters);
+}
+
+function generateFilters(filters){
+  var container = $("#container-filter");
+  $.each(filters, function(i, item) {
+    var div = $("<div>").attr("class", "filter-section").appendTo(container);
+    var title  =  $("<h5>").html(i).appendTo(div);
+    var ul = $("<ul>").appendTo(div);
+
+    $.each(item, function(k, value) {
+      var li = $("<li>").html(value).appendTo(ul);
+    });
+  })
 }
 
 //Function to toggle visibility of navigation dropdown for mobile nav
@@ -71,4 +160,9 @@ function toggleSideNav() {
       $('.nav-links-li').removeClass('show');
       $('.nav-links-li').addClass('hide');
     }
+}
+
+//Function to toggle hover container
+function toggleHover(item){
+  $(item).children(".hoverContainer").toggleClass("hoverContainer-show");
 }
